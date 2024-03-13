e9d10c46ecad8cf2e3078feca341eeddc011b498
Nuno Neto <nuno.neto.g@gmail.com>
2024-03-13 19:33:06 +0000

    The repository patterns for the storage layer that our service is going to require.

---
+++ src/repositories/clients.rs
@@ -0,0 +1,22 @@
+use std::sync::{Arc, Mutex};
+use crate::models::client::Client;
+use crate::models::ClientID;
+
+pub type StoredClient = Arc<Mutex<Client>>;
+
+/// The client repository trait, meant to represent the storage of the client
+/// models.
+pub trait TClientRepository: Send + Sync {
+
+    async fn find_client_by_id(&self, client_id: ClientID) -> Option<StoredClient>;
+
+    /// Save the changes made in this stored client instance
+    ///
+    /// In order to implement this in a given repository, we should use the Unit Of Work
+    /// pattern.
+    async fn save_client(&self, client: StoredClient);
+
+    /// Register a client that does not yet exist in the repository
+    async fn store_client(&self, client: Client) -> StoredClient;
+
+}
---
+++ src/repositories/transactions.rs
@@ -0,0 +1,29 @@
+use std::sync::{Arc, Mutex};
+
+use crate::models::TransactionID;
+use crate::models::transactions::Transaction;
+
+pub type StoredTX = Arc<Mutex<Transaction>>;
+
+/// The repository abstraction for the transaction storage layer.
+///
+/// In this case, this is meant to handle local concurrency, but it basically
+/// only supports in memory storage.
+/// At the moment, the only way I can think of to correctly support offsite repositories
+/// is to make all modifications run by this repository, which would mean we must have
+/// all of the transaction functions "mirrored" here
+pub trait TTransactionRepository: Send + Sync {
+
+    /// Find a tx by a given ID
+    async fn find_tx_by_id(&self, tx_id: TransactionID) -> Option<StoredTX>;
+
+    /// Indicate to the repository that we should save the changes done to the stored transaction
+    /// This could be done with the Unit Of Work pattern or something similar.
+    async fn save_tx(&self, tx: StoredTX);
+
+    /// Store a tx in the repository
+    ///
+    /// Store a transaction that is not in the repository into the repository
+    async fn store_tx(&self, tx: Transaction) -> StoredTX;
+
+}
